053b2827aa54a7e577e021ae0c025318b2c08cac
Leon35-fc <leon.f35@alice.it>
2025-11-18 16:53:30 +0100

    Update AllTheBooks.jsx

--- vite-project/src/components/AllTheBooks.jsx
+++ vite-project/src/components/AllTheBooks.jsx
@@ -1,31 +1,31 @@
 import { Container, Row, Col, Card } from 'react-bootstrap'
 import { Component } from 'react'
-import books from '../data/fantasy.json'
+import library from '../data/fantasy.json'
 
 class AllTheBooks extends Component {
-    state={
-        activeBook : books,
-    }
 
     render() {
         return(
             <Container>
                 <Row>
-                    <Col className='flex-wrap'>
-                        <div>
-                            <h1 className="text-center my-3">I migliori libri {books[0].category} del web!</h1>
-                            <Card>
-                                <Card.Img variant={books.title} src={books.img} />
+                <h1 className="text-center my-3">I migliori libri {library[0].category} del web!</h1>
+                    <Row className="d-flex flex-wrap justify-content-center">
+                        { library.map((book) => {
+                            return(
+                            <Card Col key={book.id} xs={12} sm={6} md={4} lg={3} className="mb-2 me-1 w-25">
+                                <Card.Img variant='book' src={book.img} />
                                 <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Title>{book.title}</Card.Title>
                                     <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
-                        </div>
-                    </Col>
+                            )
+                            })
+                        }
+                    </Row>
                 </Row>
             </Container>
         )
