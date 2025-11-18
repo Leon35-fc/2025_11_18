814e36fbafe7bec37083ef93202b31b88ddd0abd
Leon35-fc <leon.f35@alice.it>
2025-11-18 23:04:13 +0100

    graphic improvement

--- vite-project/src/components/AllTheBooks.jsx
+++ vite-project/src/components/AllTheBooks.jsx
@@ -6,13 +6,13 @@ class AllTheBooks extends Component {
 
     render() {
         return(
-            <Container>
+            <Container className='bg-primary-subtle'>
                 <Row>
-                <h1 className="text-center my-3">I migliori libri {library[0].category} del web!</h1>
+                <h1 className="text-center my-3">I migliori libri <span className='text-danger'>{library[0].category}</span> del web!</h1>
                     <Row className="d-flex flex-wrap justify-content-center">
                         { library.map((book) => {
                             return(
-                            <Card Col key={book.id} xs={12} sm={6} md={4} lg={3} className="mb-4 me-4 w-25">
+                            <Card Col key={book.id} xs={12} sm={6} md={4} lg={3} className="bg-primary shadow mb-4 me-4 w-25">
                                 <Card.Img variant='book' src={book.img} />
                                 <Card.Body>
                                     <Card.Title>{book.title}</Card.Title>
